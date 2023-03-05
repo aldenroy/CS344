156b2c46f532c0e56aab113d778bf8727af3ccc6
aroy2003 <alden.j.roy@gmail.com>
2023-03-05 08:41:23 -0800

    Server files both work.

--- program4/dec_server.c
+++ program4/dec_server.c
@@ -44,35 +44,56 @@ void decode(char message[], char key[]){ //decode the message
 	  return;
 }
 
+void setupAddressStruct(struct sockaddr_in* address, int portNumber){
+ 
+	// Clear out the address struct
+	memset((char*) address, '\0', sizeof(*address)); 
+
+	// The address should be network capable
+	address->sin_family = AF_INET;
+	// Store the port number
+	address->sin_port = htons(portNumber);
+	// Allow a client at any address to connect to this server
+	address->sin_addr.s_addr = INADDR_ANY;
+}
+
 int main(int argc, char *argv[]) //main function where server is initialized and communicates with client
 {
-	int listenSocketFD, establishedConnectionFD, portNumber, charsRead, status;
-	socklen_t sizeOfClientInfo;
+	int connectionSocket, charsRead, establishedConnectionFD, status, portNumber;
+	char buffer[256];
 	struct sockaddr_in serverAddress, clientAddress;
+	socklen_t sizeOfClientInfo = sizeof(clientAddress);
 	pid_t pid;
 
-	if (argc < 2) { fprintf(stderr, "USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
-
-																			 // Set up the address struct for this process (the server)
-	memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-	portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
-	serverAddress.sin_family = AF_INET; // Create a network-capable socket
-	serverAddress.sin_port = htons(portNumber); // Store the port number
-	serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
-
-												// Set up the socket
-	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
-	if (listenSocketFD < 0) error("ERROR opening socket");
+	// Check usage & args
+	if (argc < 2) { 
+		fprintf(stderr,"USAGE: %s port\n", argv[0]); 
+		exit(1);
+	} 
+	
+	// Create the socket that will listen for connections
+	int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket < 0) {
+		error("ERROR opening socket");
+	}
+	// Set up the address struct for the server socket
+	setupAddressStruct(&serverAddress, atoi(argv[1]));
 
 	// Enable the socket to begin listening
-	if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
+	if (bind(listenSocket, 
+			(struct sockaddr *)&serverAddress, 
+			sizeof(serverAddress)) < 0){
 		error("ERROR on binding");
+	}
+
+	// Start listening for connetions. Allow up to 5 connections to queue up
+	listen(listenSocket, 5); 
+		//here
 	while(1){ //server infinite loop
-		listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections	
 
 								   // Accept a connection, blocking if one is not available until one connects
 		sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
-		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+		establishedConnectionFD = accept(listenSocket, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
 		if (establishedConnectionFD < 0) error("ERROR on accept");
 
 		//server loop
@@ -167,6 +188,6 @@ int main(int argc, char *argv[]) //main function where server is initialized and
 		}
 		close(establishedConnectionFD); // Close the existing socket which is connected to the client
 	}
-	close(listenSocketFD); // Close the listening socket
+	close(listenSocket); // Close the listening socket
 	return 0;
 }
